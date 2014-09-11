28bf3f293623b5382b27fe917ed3ec293fd36f77
vonzhou <vonzhou@163.com>
2014-09-11 14:28:09 +0800

    OIO client socket channel factory

--- src/org/jboss/netty/channel/socket/ClientSocketChannelFactory.java
+++ src/org/jboss/netty/channel/socket/ClientSocketChannelFactory.java
@@ -7,5 +7,6 @@ import org.jboss.netty.channel.ChannelPipeline;
  * 创建客户端SocketChannel的ChannelFactory
  */
 public interface ClientSocketChannelFactory extends ChannelFactory {
+	//具体化了返回类型
     SocketChannel newChannel(ChannelPipeline pipeline);
 }
--- src/org/jboss/netty/channel/socket/oio/OioClientSocketChannelFactory.java
+++ src/org/jboss/netty/channel/socket/oio/OioClientSocketChannelFactory.java
@@ -1,88 +1,41 @@
-/*
- * Copyright 2012 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package org.jboss.netty.channel.socket.oio;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
-
-import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelPipeline;
-import org.jboss.netty.channel.group.ChannelGroup;
 import org.jboss.netty.channel.socket.ClientSocketChannelFactory;
 import org.jboss.netty.channel.socket.SocketChannel;
 import org.jboss.netty.util.ThreadNameDeterminer;
 import org.jboss.netty.util.internal.ExecutorUtil;
 
-/**
- * A {@link ClientSocketChannelFactory} which creates a client-side blocking
- * I/O based {@link SocketChannel}.  It utilizes the good old blocking I/O API
- * which is known to yield better throughput and latency when there are
- * relatively small number of connections to serve.
- *
- * <h3>How threads work</h3>
- * <p>
- * There is only one type of threads in {@link OioClientSocketChannelFactory};
- * worker threads.
- *
- * <h4>Worker threads</h4>
- * <p>
- * Each connected {@link Channel} has a dedicated worker thread, just like a
- * traditional blocking I/O thread model.
- *
- * <h3>Life cycle of threads and graceful shutdown</h3>
- * <p>
- * Worker threads are acquired from the {@link Executor} which was specified
- * when a {@link OioClientSocketChannelFactory} was created (i.e. {@code workerExecutor}.)
- * Therefore, you should make sure the specified {@link Executor} is able to
- * lend the sufficient number of threads.
- * <p>
- * Worker threads are acquired lazily, and then released when there's nothing
- * left to process.  All the related resources are also released when the
- * worker threads are released.  Therefore, to shut down a service gracefully,
- * you should do the following:
- *
- * <ol>
- * <li>close all channels created by the factory usually using
- *     {@link ChannelGroup#close()}, and</li>
- * <li>call {@link #releaseExternalResources()}.</li>
- * </ol>
- *
- * Please make sure not to shut down the executor until all channels are
- * closed.  Otherwise, you will end up with a {@link RejectedExecutionException}
- * and the related resources might not be released properly.
- *
- * <h3>Limitation</h3>
- * <p>
- * A {@link SocketChannel} created by this factory does not support asynchronous
- * operations.  Any I/O requests such as {@code "connect"} and {@code "write"}
- * will be performed in a blocking manner.
- *
- * @apiviz.landmark
+/**是一个ClientSocketChannelFactory 的实现，基于SocketChannel的阻塞方式的客户端通道。
+ * 使用的是传统的阻塞IO API，特点是能得到好的吞吐量和低延迟，当需要服务的连接数少的时候。（联系NIO和OIO的区别）
+ * 
+ * 在OioClientSocketChannelFactory中只有一种线程类型，worker threads。每个连接的通道有一个专用的worker，
+ * 就是传统的IO模型。
+ * 
+ * 当OioClientSocketChannelFactory在构造的时候传入的参数线程池对象如Executors.newCachedThreadPool()，
+ * 就是worker threads的来源。所以要保证指定的Executor 能提供足够数量的线程。
+ * 
+ * worker thread是延迟获得的（acquired lazily）并且在没有事务要处理时就会释放,所有该thread相关的资源都会释放，
+ * 所以要优雅的关闭服务（先关闭所有的Channel，调用 ChannelGroup.close()；
+ * 而后调用releaseExternalResources()释放外部资源）。
+ * 
+ *  要注意不要在所有的通道关闭前去关闭executor ，否则会发生RejectedExecutionException ，
+ *  而且有些相关资源不会正常释放。
+ *  
+ * 限制：通过OioClientSocketChannelFactory创建的SocketChannel 不支持异步操作，
+ * 任何ＩＯ操作如连接，写等都已阻塞方式执行。
  */
 public class OioClientSocketChannelFactory implements ClientSocketChannelFactory {
 
     private final Executor workerExecutor;
-    final OioClientSocketPipelineSink sink;
+    final OioClientSocketPipelineSink sink;///
     private boolean shutdownExecutor;
 
     /**
-     * Creates a new instance with a {@link Executors#newCachedThreadPool()} as worker executor.
-     *
-     * See {@link #OioClientSocketChannelFactory(Executor)}
+     * 创建一个实例
+     * 默认会使用Executors.newCachedThreadPool()来生成worker thread
      */
     public OioClientSocketChannelFactory() {
         this(Executors.newCachedThreadPool());
@@ -91,21 +44,14 @@ public class OioClientSocketChannelFactory implements ClientSocketChannelFactory
 
     /**
      * Creates a new instance.
-     *
-     * @param workerExecutor
-     *        the {@link Executor} which will execute the I/O worker threads
+     * 指定Executor来执行 I/O worker threads
      */
     public OioClientSocketChannelFactory(Executor workerExecutor) {
         this(workerExecutor, null);
     }
 
     /**
-     * Creates a new instance.
-     *
-     * @param workerExecutor
-     *        the {@link Executor} which will execute the I/O worker threads
-     * @param determiner
-     *        the {@link ThreadNameDeterminer} to set the thread names.
+     * 利用ThreadNameDeterminer来设置thread 名字
      */
     public OioClientSocketChannelFactory(Executor workerExecutor,
                                          ThreadNameDeterminer determiner) {
@@ -116,6 +62,9 @@ public class OioClientSocketChannelFactory implements ClientSocketChannelFactory
         sink = new OioClientSocketPipelineSink(workerExecutor, determiner);
     }
 
+    /**
+     * 核心方法，创建具体的通道 OioClientSocketChannel
+     */
     public SocketChannel newChannel(ChannelPipeline pipeline) {
         return new OioClientSocketChannel(this, pipeline, sink);
     }
