d54ae6cfc6169317d19e9e71079268c95bc43959
vonzhou <vonzhou@163.com>
2014-09-11 13:54:53 +0800

    client socket channel factory

--- src/org/jboss/netty/channel/socket/SocketChannel.java
+++ src/org/jboss/netty/channel/socket/SocketChannel.java
@@ -5,7 +5,9 @@ import java.net.InetSocketAddress;
 import org.jboss.netty.channel.Channel;
 
 
-//代表TCP/IP套接字通道，或者由ServerSocketChannel接收而创建的连接套接字，或者由ClientSocketChannelFactory创建
+/**
+ * 代表TCP/IP套接字通道，或者由ServerSocketChannel接收而创建的连接套接字，或者由ClientSocketChannelFactory创建
+ */
 public interface SocketChannel extends Channel {
     SocketChannelConfig getConfig();
     InetSocketAddress getLocalAddress();
